369a85ddf4975d603ab5d4be798ac6a8f3b3e5b7
FestiveCore <iCorx91@gmail.com>
2015-07-10 15:52:10 +0200

    First Push

---
+++ HardwareTemp/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace HardwareTemp {
+    partial class Form1 {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(189, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(297, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Minimum";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(421, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Maximum";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label4.Location = new System.Drawing.Point(12, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CPU";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label5.Location = new System.Drawing.Point(12, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "GPU1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label6.Location = new System.Drawing.Point(12, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "GPU2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label7.Location = new System.Drawing.Point(208, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label8.Location = new System.Drawing.Point(327, 52);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "00";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label9.Location = new System.Drawing.Point(457, 52);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 25);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "00";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label10.Location = new System.Drawing.Point(208, 102);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(34, 25);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "00";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label11.Location = new System.Drawing.Point(327, 102);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(34, 25);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "00";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label12.Location = new System.Drawing.Point(457, 102);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(34, 25);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "00";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label13.Location = new System.Drawing.Point(208, 150);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(34, 25);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "00";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.BackColor = System.Drawing.Color.Transparent;
+            this.label14.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label14.Location = new System.Drawing.Point(327, 150);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(34, 25);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "00";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.Color.Transparent;
+            this.label15.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label15.Location = new System.Drawing.Point(457, 150);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(34, 25);
+            this.label15.TabIndex = 14;
+            this.label15.Text = "00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.BackColor = System.Drawing.Color.Transparent;
+            this.label16.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label16.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label16.Location = new System.Drawing.Point(103, 9);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(56, 25);
+            this.label16.TabIndex = 15;
+            this.label16.Text = "Load";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.BackColor = System.Drawing.Color.Transparent;
+            this.label17.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label17.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label17.Location = new System.Drawing.Point(113, 52);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(34, 25);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "00";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.BackColor = System.Drawing.Color.Transparent;
+            this.label18.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label18.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label18.Location = new System.Drawing.Point(113, 102);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(34, 25);
+            this.label18.TabIndex = 17;
+            this.label18.Text = "00";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.BackColor = System.Drawing.Color.Transparent;
+            this.label19.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label19.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label19.Location = new System.Drawing.Point(113, 150);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(34, 25);
+            this.label19.TabIndex = 18;
+            this.label19.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(549, 193);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "HardwareTemp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+    }
+}
+
---
+++ HardwareTemp/Form1.cs
@@ -0,0 +1,176 @@
+﻿//  /u/FestiveCore  for  /r/pcmasterrace
+//
+// Requirements :
+// - OpenHardwareMonitor Library
+// 
+// If you are copying the project into Visual Studio, you will need 19 labels.
+// The program is made for a 4-cores CPU and 2 GPUs
+// If you want to change the number of cores, just go into the method AvgMinMax() and change the two dividers in the CPU average.
+// If you have an ATI/AMD GPU, change the code in GPU Loads and Temps in Update_Temps() method.
+// If you have only one GPU, remove all stuff with GPU2 and the variable ct that is used to switch through the 2 GPUs.
+//
+// The timer is used for the main loop as WinForms is an event driven achitecture.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using OpenHardwareMonitor;
+using OpenHardwareMonitor.Hardware;
+
+namespace HardwareTemp {
+
+    public partial class Form1 : Form {
+
+        Computer cpt = new Computer();
+
+        public static int CPU_cur = 0;
+        public static int CPU_min = 100;
+        public static int CPU_max = 0;
+
+        public static int GPU1_cur = 0;
+        public static int GPU1_min = 100;
+        public static int GPU1_max = 0;
+
+        public static int GPU2_cur = 0;
+        public static int GPU2_min = 100;
+        public static int GPU2_max = 0;
+
+        public static int CPU_Load = 0;
+        public static int GPU1_Load = 0;
+        public static int GPU2_Load = 0;
+
+
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            timer1.Start();
+            InitComp(cpt);
+        }
+
+        public void MainLoop() {
+            Update_Values(cpt);
+            AvgMinMax();
+            Display();
+        }
+        
+        public static void InitComp(Computer cmp) {
+            cmp.Open();
+            cmp.CPUEnabled = true;
+            cmp.GPUEnabled = true;
+        }
+
+        public static void Update_Values(Computer cmp) {
+
+            CPU_cur = 0;
+            CPU_Load = 0;
+            int ct = 0;
+            
+            foreach (var hardware in cmp.Hardware) {
+
+                hardware.Update();
+
+                if (ct == 0)
+                    ct++;
+                else
+                    ct--;
+
+
+                foreach (var sensor in hardware.Sensors) {
+
+                    //CPU temps & Loads
+                    if (sensor.Hardware.HardwareType == HardwareType.CPU) {
+                        if (sensor.SensorType == SensorType.Temperature && sensor.Value != null && sensor.Name != "CPU Package") {
+                            CPU_cur += (int)sensor.Value;
+                        }
+                        if (sensor.SensorType == SensorType.Load && sensor.Name != "CPU Total" && sensor.Value != null) {
+                            CPU_Load += (int)sensor.Value;
+                        }
+                    }
+                    //GPU Temps & Loads
+                    else if (sensor.Hardware.HardwareType == HardwareType.GpuNvidia) {  //Change by HardwareType.GpuAti if you have an AMD/ATI GPU
+                        if (ct != 0) {
+                            if (sensor.SensorType == SensorType.Temperature && sensor.Value != null)
+                                GPU1_cur = (int)sensor.Value;
+                            if (sensor.SensorType == SensorType.Load && sensor.Name == "GPU Core" && sensor.Value != null)
+                                GPU1_Load = (int)sensor.Value;
+                        } else {
+                            if (sensor.SensorType == SensorType.Temperature && sensor.Value != null)
+                                GPU2_cur = (int)sensor.Value;
+                            if (sensor.SensorType == SensorType.Load && sensor.Name == "GPU Core" && sensor.Value != null)
+                                GPU2_Load = (int)sensor.Value;
+                        }
+
+
+
+                    }
+                    
+                }
+            }
+
+
+        }
+
+        public static void AvgMinMax() {
+
+            //CPU Average, divided by 4 for a 4-cores CPU
+            CPU_cur = CPU_cur / 4;
+            CPU_Load = CPU_Load / 4;
+
+            //MinMax CPU
+            if (CPU_cur < CPU_min)
+                CPU_min = CPU_cur;
+            if (CPU_cur > CPU_max)
+                CPU_max = CPU_cur;
+
+            //MinMax GPU1
+            if (GPU1_cur < GPU1_min)
+                GPU1_min = GPU1_cur;
+            if (GPU1_cur > GPU1_max)
+                GPU1_max = GPU1_cur;
+
+            //MinMax GPU2
+            if (GPU2_cur < GPU2_min)
+                GPU2_min = GPU2_cur;
+            if (GPU2_cur > GPU2_max)
+                GPU2_max = GPU2_cur;
+        }
+
+        public void Display() {           
+
+            //CPU
+            label7.Text = CPU_cur.ToString() + " °C";
+            label8.Text = CPU_min.ToString() + " °C";
+            label9.Text = CPU_max.ToString() + " °C";
+
+            //GPU 1
+            label10.Text = GPU1_cur.ToString() + " °C";
+            label11.Text = GPU1_min.ToString() + " °C";
+            label12.Text = GPU1_max.ToString() + " °C";
+
+            //GPU 2
+            label13.Text = GPU2_cur.ToString() + " °C";
+            label14.Text = GPU2_min.ToString() + " °C";
+            label15.Text = GPU2_max.ToString() + " °C";
+
+            //Loads
+            label17.Text = CPU_Load.ToString() + " %";
+            label18.Text = GPU1_Load.ToString() + " %";
+            label19.Text = GPU2_Load.ToString() + " %";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            MainLoop();
+        }
+                
+    }
+}
